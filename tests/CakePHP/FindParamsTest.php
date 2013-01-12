e7230b4c99369a947424e8df08259f4b04c03a4e
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2013-01-13 02:29:27 +0700

    Add limit, page, offset and callbacks params. #5

--- tests/CakePHP/FindParamsTest.php
+++ tests/CakePHP/FindParamsTest.php
@@ -45,4 +45,24 @@ class FindParamsTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($expected, $actual);
 	}
 
+	public function testSetFlatValueLikeLimitPageOffsetAndCallbacks() {
+		$expected = array(
+			'limit'      => 100,
+			'page'       => 1,
+			'offset'     => 10,
+			'callbacks'  => true,
+		);
+
+		$findParams = new OopConfig_CakePHP_FindParams();
+		$actual     = $findParams
+			->limit(100)
+			->page(1)
+			->offset(10)
+			->callbacks(true)
+			->get()
+		;
+
+		$this->assertEquals($expected, $actual);
+	}
+
 }
